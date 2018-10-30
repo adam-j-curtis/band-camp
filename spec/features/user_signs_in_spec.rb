96ae34feaf8d67d5e97b8a4e79e89066855870fb
Adam Curtis <adamjcurtis@gmail.com>
2018-10-30 12:13:56 -0400

    Removing factorybot stubs in spec/features

--- spec/features/user_signs_in_spec.rb
+++ spec/features/user_signs_in_spec.rb
@@ -6,12 +6,14 @@ feature 'user signs in', %Q{
   So that I can regain access to my account
 } do
   scenario 'specify valid credentials' do
-    user = FactoryBot.create(:user)
+    # user = FactoryBot.create(:user)
 
     visit new_user_session_path
 
-    fill_in 'Email', with: user.email
-    fill_in 'Password', with: user.password
+    # fill_in 'Email', with: user.email
+    fill_in 'Email', with: "vegas.rosadas@email.com"
+    # fill_in 'Password', with: user.password
+    fill_in 'Password', with: "password"
 
     click_button 'Log in'
 
--- spec/features/user_signs_out_spec.rb
+++ spec/features/user_signs_out_spec.rb
@@ -11,12 +11,14 @@ feature 'user signs out', %Q{
   #   forgotten on the machine I'm using
 
   scenario 'authenticated user signs out' do
-    user = FactoryBot.create(:user)
+    # user = FactoryBot.create(:user)
 
     visit new_user_session_path
 
-    fill_in 'Email', with: user.email
-    fill_in 'Password', with: user.password
+    # fill_in 'Email', with: user.email
+    fill_in 'Email', with: "vegas.rosadas@email.com"
+    # fill_in 'Password', with: user.password
+    fill_in 'Password', with: "password"
 
     click_button 'Log in'
 
