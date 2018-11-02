570d44ac0c564bf3ae621c920b5729ab4a30a58d
Adam Curtis <adamjcurtis@gmail.com>
2018-11-02 15:12:30 -0400

    Updated model test, feature test, user model, sign up page.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,37 +6,43 @@ RSpec.describe User, :type => :model do
   # fill_in 'Password confirmation', with: 'password'
 
   it "is not valid without a First Name" do
-    test_user = User.create(first_name: nil, last_name: "Vega", school_name: "Eldridge Academy", city: "Buenos Aires", email: "vegas.rosadas@example.com", password: "password")
+    test_user = User.create(first_name: nil, last_name: "Vega", school_name: "Eldridge Academy", city: "Buenos Aires", email: "vegas.rosadas@example.com", password: "password", position: "teacher")
     expect(test_user).to_not be_valid
     expect(test_user.errors.messages).to eq(:first_name=>["can't be blank"])
   end
 
   it "is not valid without a Last Name" do
-    test_user = User.create(first_name: "June", last_name: nil, school_name: "Eldridge Academy", city: "Buenos Aires", email: "vegas.rosadas@example.com", password: "password")
+    test_user = User.create(first_name: "June", last_name: nil, school_name: "Eldridge Academy", city: "Buenos Aires", email: "vegas.rosadas@example.com", password: "password", position: "teacher")
     expect(test_user).to_not be_valid
     expect(test_user.errors.messages).to eq(:last_name=>["can't be blank"])
   end
 
   it "is not valid without a School Name" do
-    test_user = User.create(first_name: "June", last_name: "Vega", school_name: nil, city: "Buenos Aires", email: "vegas.rosadas@example.com", password: "password")
+    test_user = User.create(first_name: "June", last_name: "Vega", school_name: nil, city: "Buenos Aires", email: "vegas.rosadas@example.com", password: "password", position: "teacher")
     expect(test_user).to_not be_valid
     expect(test_user.errors.messages).to eq(:school_name=>["can't be blank"])
   end
 
   it "is not valid without a City" do
-    test_user = User.create(first_name: "June", last_name: "Vega", school_name: "Eldridge Academy", city: nil, email: "vegas.rosadas@example.com", password: "password")
+    test_user = User.create(first_name: "June", last_name: "Vega", school_name: "Eldridge Academy", city: nil, email: "vegas.rosadas@example.com", password: "password", position: "teacher")
     expect(test_user).to_not be_valid
     expect(test_user.errors.messages).to eq(:city=>["can't be blank"])
   end
 
   it "is not valid without an Email" do
-    test_user = User.create(first_name: "June", last_name: "Vega", school_name: "Eldridge Academy", city: "Buenos Aires", email: nil, password: "password")
+    test_user = User.create(first_name: "June", last_name: "Vega", school_name: "Eldridge Academy", city: "Buenos Aires", email: nil, password: "password", position: "teacher")
     expect(test_user).to_not be_valid
     expect(test_user.errors.messages).to eq(:email=>["can't be blank"])
   end
 
   it "is not valid without a Password" do
-    test_user = User.create(first_name: "June", last_name: "Vega", school_name: "Eldridge Academy", city: "Buenos Aires", email: "vegas.rosadas@example.com", password: nil)
+    test_user = User.create(first_name: "June", last_name: "Vega", school_name: "Eldridge Academy", city: "Buenos Aires", email: "vegas.rosadas@example.com", password: nil, position: "teacher")
+    expect(test_user).to_not be_valid
+    expect(test_user.errors.messages).to eq(:password=>["can't be blank"])
+  end
+
+  it "is not valid without a Position" do
+    test_user = User.create(first_name: "June", last_name: "Vega", school_name: "Eldridge Academy", city: "Buenos Aires", email: "vegas.rosadas@example.com", password: "password", position: nil)
     expect(test_user).to_not be_valid
     expect(test_user.errors.messages).to eq(:password=>["can't be blank"])
   end
