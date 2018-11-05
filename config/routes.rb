eb9497bc2171199914346e4f00d8840636e28459
Adam Curtis <adamjcurtis@gmail.com>
2018-11-04 21:27:27 -0500

    Basic teacher show page, listing their students with links in alpha order, generated.

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
 
   resources :users, only: [ :index, :show ]
 
+
     # namespace :api do
     #   namespace :v1 do
     #     resources :users, only: [:index, :show]
