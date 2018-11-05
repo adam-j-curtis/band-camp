97a0ea3919700cd6e9f6656bf69d92b197bf5775
Adam Curtis <adamjcurtis@gmail.com>
2018-11-05 13:01:38 -0500

    Incomplete work activating react show functions, react recorder.

--- app/javascript/react/components/app.js
+++ app/javascript/react/components/app.js
@@ -1,11 +1,12 @@
-// import { Router, browserHistory, Route, IndexRoute } from 'react-router';
-// import StudentShow from '../containers/StudentShow'
 import React from 'react'
+import { Router, browserHistory, Route, IndexRoute } from 'react-router';
+
+import StudentShow from '../containers/StudentShow'
 
 export const App = (props) => {
   return (
     <Router onUpdate={() => window.scrollTo(0, 0)} history={browserHistory}>
-      <Route path='/user/:id' component={StudentShow}/>
+      <Route path='/users/:id' component={StudentShow}/>
     </Router>
   )
 }
