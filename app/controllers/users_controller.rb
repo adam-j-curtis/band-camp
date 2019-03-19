47679a24562d0496ba5bf8dd7e50bb51cb8da233
Adam Curtis <adamjcurtis@gmail.com>
2019-03-19 14:48:07 -0400

    Creating a student now sends to the new student page with appropriate messages for empty bands and assignments.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,6 +6,7 @@ class UsersController < ApplicationController
   def index
     @users = User.all
     @students = User.where(position: "student")
+    @teachers = User.where(position: "teacher")
   end
 
   def show
